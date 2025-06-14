c2a01ae0d3a36213fb43d46a1e9a30d429a63bae
Caio <caiosantiago0987sg@gmail.com>
2025-06-14 13:45:00 -0300

    commit inicial
    avançado no Projeto Filmes.

--- CLIENTES.cob
+++ CLIENTES.cob
@@ -1,15 +1,32 @@
        IDENTIFICATION DIVISION.
        PROGRAM-ID. CLIENTES.
       ************************************
-      * OBJETIVO: SISTEMA DE GESTAO DE CLIENTES
-      * AUTHOR: CAIO
+      * AUTHOR: CAIO                      EMPRESA: X
+      *OBJETIVO: SISTEMA DE GESTAO DE FLIMES
+      *Data:
       **************************************
        ENVIRONMENT DIVISION.
-       DATA DIVISION.
+       INPUT-OUTPUT SECTION.
+       FILE-CONTROL.
+       SELECT CLIENTES ASSIGN TO
+       'C:\Users\Windows\Cobol\ProjetoFinalCobol\CLIENTES.DAT'
+       ORGANIZATION IS INDEXED
+       ACCESS MODE IS RANDOM
+       FILE STATUS IS CLIENTES-STATUS
+       RECORD KEY IS CLIENTES-CHAVE.
+           DATA DIVISION.
+           FILE SECTION.
+       FD CLIENTES.
+       01 CLIENTES-REG.
+       05 CLIENTES-CHAVE.
+       10 CLIENTES-FONE PIC 9(09).
+       05 CLIENTES-NOME PIC X(30).
+       05 CLIENTES-EMAIL PIC X(40).
        WORKING-STORAGE SECTION.
        77 WRK-OPCAO PIC X(1).
        77 WRK-MODULO PIC X(25).
        77 WRK-TECLA PIC X(1).
+       77 CLIENTES-STATUS PIC 9(02).
        SCREEN SECTION.
        01 TELA.
              05 LIMPA-TELA.
@@ -30,6 +47,23 @@
        05 LINE 12 COLUMN 15 VALUE 'X - SAIDA'.
        05 LINE 13 COLUMN 15 VALUE 'OPCAO........:'.
        05 LINE 13 COLUMN 28 USING WRK-OPCAO.
+
+
+
+
+
+       01 TELA-REGISTRO.
+             05 CHAVE FOREGROUND-COLOR 2.
+               10 LINE 10 COLUMN 10 VALUE 'TELEFONE'.
+               10 COLUMN PLUS 2 PIC 9(09) USING CLIENTES-FONE
+                   BLANK WHEN ZEROS.
+             05 SS-DADOS.
+               10 LINE 11 COLUMN 10 VALUE 'NOME....'.
+               10 COLUMN PLUS 2 PIC X(30) USING CLIENTES-NOME.
+               10 LINE 12 COLUMN 10 VALUE 'EMAIL...'.
+               10 COLUMN PLUS 2 PIC X(40) USING CLIENTES-EMAIL.
+
+
        PROCEDURE DIVISION.
        0001-PRINCIPAL SECTION.
        PERFORM 1000-INICIAR.
@@ -37,6 +71,12 @@
        PERFORM 3000-FINALIZAR.
        STOP RUN.
        1000-INICIAR.
+            OPEN I-O CLIENTES
+           IF CLIENTES-STATUS = 35 THEN
+           OPEN OUTPUT CLIENTES
+           CLOSE CLIENTES
+           OPEN I-O CLIENTES
+           END-IF.
            DISPLAY TELA.
            ACCEPT MENU.
            2000-PROCESSAR.
@@ -58,7 +98,7 @@
                    END-EVALUATE.
        MOVE 'MODULO - INCLUSAO' TO WRK-MODULO.
        3000-FINALIZAR.
-        CONTINUE.
+       CLOSE CLIENTES.
 
 
 
@@ -69,4 +109,7 @@
        5000-INCLUIR.
        MOVE 'MODULO - INCLUSAO' TO WRK-MODULO.
        DISPLAY TELA.
-       ACCEPT WRK-TECLA AT 1620.
+       ACCEPT TELA-REGISTRO.
+       WRITE CLIENTES-REG.
+       DISPLAY TELA.
+           ACCEPT MENU.
